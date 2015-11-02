67b6b7f0d69d301b551c9790312e8d1cc77b96bd
Mike Rooijackers <info@mikerooijackers.nl>
2015-11-02 10:51:35 +0100

    tableview wordt gevuld met test data

---
+++ Standup/Standup/HistoryTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HistoryTableViewController.swift
+//  Standup
+//
+//  Created by Mike Rooijackers on 02-11-15.
+//  Copyright © 2015 SMIOS7 Healthkit. All rights reserved.
+//
+
+import Foundation
---
+++ Standup/Standup/HistroyViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HistroyViewController.swift
+//  Standup
+//
+//  Created by Mike Rooijackers on 02-11-15.
+//  Copyright © 2015 SMIOS7 Healthkit. All rights reserved.
+//
+
+import Foundation
