34829c9a1cd9362910c5d402b5cedd5ecd9ce095
Mike Rooijackers <info@mikerooijackers.nl>
2015-11-12 12:29:48 +0100

    Facebook login met automatisch check of je ingelogd bent en door verwijzing naar tabbar. als je niet bent in gelogd dan kom je bij het login scherm.

--- Standup/Standup/ViewController.swift
+++ Standup/Standup/ViewController.swift
@@ -10,15 +10,28 @@ import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    @IBOutlet weak var loginButton: FBSDKLoginButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if (FBSDKAccessToken.currentAccessToken() == nil) {
-            print("user is not logged in")
-        } else {
-            print("user is logged in")
+        loginButton.delegate = self
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil) {
+            
+            let tabBarController = self.storyboard?.instantiateViewControllerWithIdentifier("TabBarController") as! UITabBarController
+            
+            //let todayViewControllerNav = UINavigationController(rootViewController: todayViewController)
+            
+            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            
+            appDelegate.window?.rootViewController = tabBarController
         }
     }
 
@@ -27,6 +40,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if (error != nil) {
+            print(error.localizedDescription)
+            return
+        }
+        
+        if let userToken = result.token {
+            //Get user access token
+            let token:FBSDKAccessToken = result.token
+            
+            print("Token = \(FBSDKAccessToken.currentAccessToken().tokenString)")
+            print("User ID = \(FBSDKAccessToken.currentAccessToken().userID)")
+            
+            let todayViewController = self.storyboard?.instantiateViewControllerWithIdentifier("TodayViewController") as! TodayViewController
+            
+            let todayViewControllerNav = UINavigationController(rootViewController: todayViewController)
+            
+            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            
+            appDelegate.window?.rootViewController = todayViewControllerNav
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("user is logged out")
+    }
 
 }
 
