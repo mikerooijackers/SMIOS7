3f9008a63ee39ff2f9889d20c4120f50c9b89612
Mike Rooijackers <info@mikerooijackers.nl>
2015-11-12 10:25:44 +0100

    Facebook werkt met token. alleen login moet nog.

--- Standup/Standup/ViewController.swift
+++ Standup/Standup/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if (FBSDKAccessToken.currentAccessToken() == nil) {
+            print("user is not logged in")
+        } else {
+            print("user is logged in")
+        }
     }
 
     override func didReceiveMemoryWarning() {
